9213ee17e95da78f9a71835b5546425692ffe985
bicho <josebryanomar2004@gmail.com>
2026-03-02 14:35:04 -0600

    Arquitectura: Configuración de contenedores Docker para Backend, Frontend y DB

---
+++ Defensoria-back/Dev/src/main/java/com/ipn/escom/defensoria/DefensoriaApplication.java
@@ -0,0 +1,13 @@
+package com.ipn.escom.defensoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefensoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DefensoriaApplication.class, args);
+	}
+
+}
---
+++ Defensoria-back/Dev/src/test/java/com/ipn/escom/defensoria/DefensoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipn.escom.defensoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DefensoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
