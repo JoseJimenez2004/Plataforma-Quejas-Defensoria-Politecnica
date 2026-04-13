8c18006a91a7042d8ca48f4f677255bdc478d50c
bicho <josebryanomar2004@gmail.com>
2026-04-13 14:01:23 -0600

    Limpieza de Repo y Homologacion

--- Defensoria-back/consulta/consulta/src/main/java/ipn/escom/defensoria/consulta/repository/ConsultaRepository.java
+++
@@ -1,9 +0,0 @@
-package ipn.escom.defensoria.consulta.repository;
-
-import ipn.escom.defensoria.consulta.entity.ConsultaEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ConsultaRepository extends JpaRepository<ConsultaEntity, String>{
-}
