7855b6535225d6a36a43e4a59b0bf6e3380daf84
bicho <josebryanomar2004@gmail.com>
2026-06-17 02:19:42 -0600

    Actualizacion de Base de Datos, POM y SecurityConfig

--- Backend/administracion/src/main/java/ipn/escom/defensoria/administracion/config/SecurityConfig.java
+++ Backend/administracion/src/main/java/ipn/escom/defensoria/administracion/config/SecurityConfig.java
@@ -6,7 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
@@ -20,8 +20,9 @@ import java.util.List;
 public class SecurityConfig {
 
     @Bean
+    @SuppressWarnings("deprecation")
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return NoOpPasswordEncoder.getInstance();
     }
 
     @Bean
